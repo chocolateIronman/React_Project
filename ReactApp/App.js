b25daf23a2064a3c3f82ca7043c16f5fae6f3f2f
Lucifer <chocolateironman98@gmail.com>
2020-03-05 17:48:34 +0000

    test push

---
+++ ReactApp/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+class HelloWorldApp extends Component {
+ render(){
+ return (
+ <View>
+ <Text>Hello</Text>
+ <Text>Hello</Text><Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ <Text>Hello</Text>
+ 
+ </View>
+ );
+ }
+}
+export default HelloWorldApp
