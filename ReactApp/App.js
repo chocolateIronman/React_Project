2b9678b28bdf85c7936b3c0742e2a0bd240d4ef3
Lucifer <chocolateironman98@gmail.com>
2020-03-05 19:49:50 +0000

    simple nav

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -1,38 +1,20 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-class HelloWorldApp extends Component {
- render(){
- return (
- <View>
- <Text>Hello</Text>
- <Text>Hello</Text><Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- <Text>Hello</Text>
- 
- </View>
- );
- }
+import React, {Component} from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack=createStackNavigator();
+
+const HomeScreen=(props)=>(
+  <View>
+    <Text>Home Screen</Text>
+  </View>
+)
+export default function App(){
+  return <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
 }
-export default HelloWorldApp
