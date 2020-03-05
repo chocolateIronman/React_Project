9920dc3c7c9365b6e845590c811943b624c3b60d
Lucifer <chocolateironman98@gmail.com>
2020-03-05 20:01:46 +0000

    added login button in nav

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -1,20 +1,34 @@
 import React, {Component} from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
-const Stack=createStackNavigator();
+const HomeScreen = ({navigation}) => {
+  navigation.setOptions({
+    headerRight: () => (
+      <Button title="Login" onPress={() => navigation.navigate('Login')}></Button>
+    )
+  })
+  return (
+    <View>
+      <Text>Chits follow:</Text>
 
-const HomeScreen=(props)=>(
+    </View>
+  );
+};
+const LoginScreen = (props) => (
   <View>
-    <Text>Home Screen</Text>
+    <Text>Login:</Text>
   </View>
 )
-export default function App(){
+
+export default function App() {
   return <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+      <Stack.Screen name="Chittr" component={HomeScreen}></Stack.Screen>
+      <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
     </Stack.Navigator>
   </NavigationContainer>
 }
