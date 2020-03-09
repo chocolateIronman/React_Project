5a180795656971e176a43806a73900c3b7800294
Lucifer <chocolateironman98@gmail.com>
2020-03-09 13:48:42 +0000

    importing screens

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Card} from 'react-native-shadow-cards';
+import {HomeScreen} from './screens/HomeScreen'
 
 const Tab = createBottomTabNavigator();
 
@@ -23,62 +24,13 @@ function LoginScreen() {
   );
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      chitsListData: []
-    }
-  }
 
-  getData() {
-    return fetch('http://10.0.2.2:3333/api/v0.0.5/chits', {
-      headers: {
-        'Content-type': 'application/json',
-        //'X-Authorization' : KEY
-      }
-    }).then((response) => response.json()).then((responseJson) => {
-      this.setState({isLoading: false, chitsListData: responseJson});
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
-  componentDidMount() {
-    this.getData();
-  }
-
-  render() {
-    return (
-      <View>
-        <Text style={{textAlign: 'center', color: '#8ceded', fontSize:35}}>Chittr</Text>
-        <FlatList
-          data={this.state.chitsListData}
-          renderItem={({item}) => 
-          <Card style={{padding: 10, margin: 10, alignSelf:'center'}}>
-          <View style={{flex: 1, flexDirection: 'row'}}>
-            <Image style={{width: 50, height: 50, borderRadius: 50}}
-          source={{uri: 'http://10.0.2.2:3333/api/v0.0.5/user/'+item.user.user_id+'/photo'}}></Image>
-           
-            <View style={{marginTop:15, marginLeft:10}}>
-              <Text style={{fontWeight: 'bold'}}>{item.user.given_name} {item.user.family_name}</Text>
-              <Text style={{marginTop:5,maxWidth:270}}>{item.chit_content}</Text>
-            </View>
-            </View>
-          </Card>}
-          keyExtractor={({
-          id
-        }, index) => id}/>
-      </View>
-    );
-  }
-}
 
 export default function Navigation() {
   return <NavigationContainer>
     <Tab.Navigator>
       
-      <Tab.Screen name="Home" component={App}></Tab.Screen>
+      <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
       <Tab.Screen name="Search" component={SearchScreen}></Tab.Screen>
       <Tab.Screen name="Login" component={LoginScreen}></Tab.Screen>
     </Tab.Navigator>
